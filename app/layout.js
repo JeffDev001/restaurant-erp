e6bc9ed3f6afbc24b375508dcdeb60641d79e8c5
JeffDev001 <jeffreyagyei18@gmail.com>
2026-09-04 13:40:10 +0000

    Title changed to Devins Restaurant ERP

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Restaurant ERP",
+  title: "Devins Restaurant ERP",
   description: "Welcome to Devins Restaurant ERP",
 };
 
